4362a83b952e026f0913e2f380d4bfa2df3795f8
Dima Loburets <loburets@a2design.biz>
2017-06-19 15:34:51 +0600

    Added article seed

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,6 +5,6 @@ $factory->define(App\Models\Article::class, function (Faker\Generator $faker) {
 
     return [
         'name' => $faker->sentence(),
-        'text' => $faker->text(),
+        'text' => $faker->text(500),
     ];
 });
