6d24182a8247fd6f0e73347fc68b9717fa140c8d
Dima Loburets <loburets@a2design.biz>
2017-06-19 19:28:11 +0600

    Added draft of routes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.VueRouter = require('vue-router');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -17,12 +18,28 @@ window.Vue = require('vue');
 
 /**
  * TODO
- * routes
  * vuex
  */
-Vue.component('article-list', require('./components/Article-list.vue'));
+Vue.component('article-list', require('./components/Article-list.vue'))
+//todo import router-link?
+// Vue.component('router-link', require('vue-router'))
+
+Vue.use(VueRouter)
+
+// TODO These can be imported from other files
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<article-list></article-list>' }
+
+const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
 
 const app = new Vue({
-    el: '#app',
-    template: '<article-list></article-list>',
-});
+    // router,
+    template: '<router-link to="/foo">Go to Foo</router-link> <router-link to="/bar">Go to Bar</router-link>'
+}).$mount('#app')
